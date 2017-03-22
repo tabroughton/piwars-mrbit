e0cb41dd5035ee9c83f03fd227a64fbe174d978c
tabroughton <tom.a.broughton@gmail.com>
2017-03-22 23:33:07 +0000

    cleanup to PID and other files

--- python/mrBit_pid.py
+++ python/mrBit_pid.py
@@ -1,185 +1,208 @@
-"""
-/**********************************************************************************************
- * Arduino PID Library - Version 1.1.1
- * by Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com
- *
- * This Library is licensed under a GPLv3 License
- **********************************************************************************************/
-"""
+#   Python PID library
+#   Atuhor: Tom Broughton (@dpolymath)
+#   Date: 10/03/2017
+#
+#   A library to control output via PID,
+#   inspired tutorials and C++ code by Brett Beauregard brettbeauregard.com
+#
+# #############################################################################
+#   MIT License
+#
+#   Copyright (c) 2017 Tom Broughton https://github.com/tabroughton/
+#
+#   Permission is hereby granted, free vof charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all
+#   copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+###############################################################################
 import time
 
-class mrBit_pid:
+class MrBit_PID:
     AUTOMATIC = 1
     MANUAL = 0
     DIRECT = 0
     REVERSE = 1
 
-    """
-    millis returns milliseconds
-    """
-    def millis(self):
+    @classmethod
+    def millis(cls):
+        """ returns time now in milliseconds """
         return int(round(time.time() * 1000))
 
-    """
-    /*Constructor (...)*********************************************************
-     *    The parameters specified here are those for for which we can't set up
-     *    reliable defaults, so we need to have the user set them.
-     ***************************************************************************/
-    """
 
-    def __init__(self, Setpoint,  Kp,  Ki,  Kd, Input=0, Output=0, ControllerDirection=1, SampleTime=100):
-
-        self.myInput = Input
-        self.myOutput = Output
-        self.mySetpoint = Setpoint
-        self.inAuto = False
-
-        self.SetOutputLimits(0,255)			#default output limit corresponds to
-        									    #the arduino pwm limits
-
+    def __init__(self, Setpoint,  Kp,  Ki,  Kd, Output,
+                    ControllerDirection=0,
+                    SampleTime=100,
+                    MinOutput=0,
+                    MaxOutput=255):
+        """ The parameters specified here are those for for which we can't set up
+            reliable defaults, so we need to have the user set them.
+            Kp, Ki, Kd are PID Constants
+            Setpoint is the target and Input is current position
+            Output the base value of Output
+            Controller direction can be DIRECT or REVERSE (see global onstants)
+        """
+        self.set_output(Output)
         self.sampleTime = SampleTime
-        print(self.sampleTime)					#default Controller Sample Time is 0.1 seconds
-
-        self.SetControllerDirection(ControllerDirection)
-        self.SetTunings(Kp, Ki, Kd)
-
+        self.controller_direction = ControllerDirection
+        self.set_tunings(Kp, Ki, Kd)
+        self.set_output_limits(MinLimit, MaxLimit)
+        self.mySetpoint = Setpoint
+        self.inAuto = True
         self.lastTime = self.millis() - self.sampleTime
+        self.lastInput = 0
 
 
-    """
-    /* Compute() **********************************************************************
-     *     This, as they say, is where the magic happens.  this function should be called
-     *   every time "void loop()" executes.  the function will decide for itself whether a new
-     *   pid Output needs to be computed.  returns true when the output is computed,
-     *   false when nothing has been done.
-     **********************************************************************************/
-    """
-    def Compute(self, Input):
-        if not self.inAuto: return False;
+    def compute(self, Input):
+        """ In the words of Brett Beauregard "This, as they say, is where the
+            magic happens.""  This function should be called
+            every time a loop of the process executes - for exaple each time we
+            loop around a main processing application controlling a robot or other
+            machine.  The function will decide for itself whether a new
+            pid Output needs to be computed based on current input constants set,
+            previous states and whether the sample time has been reached.
+            Parameter Input = the current position from sensor readings.
+            Returns true when the output is computed false when nothing has been
+            done. Accessing new values from the pid comes from myOuput - Use
+            get_output()
+        """
+        self.myInput = Input
+        if not self.inAuto: return False
         now = self.millis()
         timeChange = (now - self.lastTime)
         if timeChange >= self.sampleTime:
-
             # Compute all the working error variables
-            self.Input = Input
-            error = self.mySetpoint - self.Input
+            error = self.mySetpoint - self.myInput
             self.ITerm += (self.ki * error)
             if self.ITerm > self.outMax:
                 self.ITerm = self.outMax
             elif self.ITerm < self.outMin:
                 self.ITerm = self.outMin
-
-            dInput = self.Input - self.lastInput
+            dInput = self.myInput - self.lastInput
 
             #compute PID Output
             output = (self.kp * error) + self.ITerm - (self.kd * dInput)
-
             if output > self.outMax:
                 output = self.outMax
             elif output < self.outMin:
                 output = self.outMin
-
             self.myOutput = output
+            #print("input: %d, error: %d, output: %d" % (self.myInput, error, self.myOutput))
 
-            #Remember some variables for next time
-            self.lastInput = self.Input
+            #remember these for next PID loop
+            self.lastInput = self.myInput
             self.lastTime = now
             return True
         else:
            return False
 
-    """
-    /* SetTunings(...)*************************************************************
-     * This function allows the controller's dynamic performance to be adjusted.
-     * it's called automatically from the constructor, but tunings can also
-     * be adjusted on the fly during normal operation
-     ******************************************************************************/
-     """
-    def SetTunings(self, Kp, Ki, Kd):
 
+    def set_tunings(self, Kp, Ki, Kd):
+        """ This function allows the controller's dynamic performance to be
+            adjusted. It's called automatically from the constructor, but tunings
+            can also be adjusted on the fly during normal operation (for example)
+            when detecting cliff edges on sensor readings.
+        """
         if Kp<0 or Ki<0 or Kd<0: return
-
         self.dispKp = Kp
         self.dispKi = Ki
         self.dispKd = Kd
 
         SampleTimeInSec = self.sampleTime / 1000.0
-        self.kp = Kp
         self.ki = Ki * SampleTimeInSec
         self.kd = Kd / SampleTimeInSec
 
-        if self.controllerDirection == mrBit_pid.REVERSE:
+        if self.controller_direction == MrBit_PID.REVERSE:
             self.kp = -self.kp
             self.ki = -self.ki
             self.kd = -self.kd
+        print("Constants Set Kp: %f, Ki: %f, Kd: %f" % (self.kp, self.ki, self.kd))
 
-    """
-    /* setOutput() **********************************
-    * manually set the  Output.
-    * ***********************************************
-    """
-    def SetOutput(self, Output):
-        self.myOutput = Output
 
-    """
-    /* SetSampleTime(...) *********************************************************
-     * sets the period, in Milliseconds, at which the calculation is performed
-     ******************************************************************************/
-    """
-    def SetSampleTime(self,newSampleTime):
-        if newSampleTime > 0:
-            ratio  = newSampleTime / self.sampleTime
+    def update_controller_direction(ControllerDirection):
+        """ The PID will either be connected to a DIRECT acting process
+            (+Output leads to +Input) or a REVERSE acting process (+Output leads
+            to -Input.)  we need to know which one, because otherwise we may
+            increase the output when we should be decreasing.  This is managed
+            via constructor and set tunings but when updating the direction we
+            need to make sure the right constants are updated.
+        """
+        if self.inAuto and self.controllerDirection != ControllerDirection:
+            self.kp = -self.kp
+            self.ki = -self.ki
+            self.kd = -self.kd
+        self.controllerDirection = ControllerDirection
+
+
+    def update_sample_time(self, NewSampleTime):
+        """ updates the period, in Milliseconds, at which the calculation is
+            performed only used if sampleTime is already set and not needing
+            resetting.
+        """
+        if NewSampleTime > 0:
+            ratio  = NewSampleTime / self.sampleTime
             self.ki *= ratio
             self.kd /= ratio
-            self.sampleTime = newSampleTime
+            self.sampleTime = NewSampleTime
 
 
-    """
-    /* SetOutputLimits(...)****************************************************
-     *     This function will be used far more often than SetInputLimits.  while
-     *  the input to the controller will generally be in the 0-1023 range (which is
-     *  the default already,)  the output will be a little different.  maybe they'll
-     *  be doing a time window and will need 0-8000 or something.  or maybe they'll
-     *  want to clamp it from 0-125.  who knows.  at any rate, that can all be done
-     *  here.
-     **************************************************************************/
-     """
-    def SetOutputLimits(self, MinLimit, MaxLimit):
+    def set_output(self, Output):
+        """ manually set the output if needing to change (eg. speed of a motor)
+            during operation.
+        """
+        self.myOutput = Output
+
+
+    def set_output_limits(self, MinLimit, MaxLimit):
+        """ Output limits can be set to ensure that the PID doesn't return values
+            outside that which can be used or are desirable.  Default limits set
+            in the Library are 255 max and 0, to match that of the PWM we are
+            typically using (in Arduino and GrovePi+) cases for Mr Bit change.
+            If using wiringpi you may want to set these limits to 1023.
+        """
         if MinLimit >= MaxLimit: return
         self.outMin = MinLimit
         self.outMax = MaxLimit
 
         if self.inAuto:
-           if self.myOutput > self.outMax:
-               self.myOutput = self.outMax
-           elif self.myOutput < self.outMin:
-               self.myOutput = self.outMin
+            if self.myOutput > self.outMax:
+                self.myOutput = self.outMax
+            elif self.myOutput < self.outMin:
+                self.myOutput = self.outMin
 
-           if self.ITerm > self.outMax:
-               self.ITerm = self.outMax
-           elif self.ITerm < self.outMin:
-               self.ITerm = self.outMin
+            if hasattr(self, "ITerm"):
+                if self.ITerm > self.outMax:
+                    self.ITerm = self.outMax
+                elif self.ITerm < self.outMin:
+                    self.ITerm = self.outMin
 
-    """
-    /* SetMode(...)****************************************************************
-     * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
-     * when the transition from manual to auto occurs, the controller is
-     * automatically initialized
-     ******************************************************************************/
-    """
-    def SetMode(self, mode):
-        if mode == mrBit_pid.AUTOMATIC and self.inAuto == False:
-            #we just went from manual to auto
-            self.Initialize()
+
+    def set_mode(self, mode):
+        """ Allows the controller Mode to be set to manual (0) or Automatic
+            (non-zero) - when the transition from manual to auto occurs,
+            the controller is automatically initialized.
+        """
+        if mode == MrBit_PID.AUTOMATIC and self.inAuto == False:
+            self.reinitialse()
         self.inAuto = mode
 
-    """
-    /* Initialize()****************************************************************
-     *	does all the things that need to happen to ensure a bumpless transfer
-     *  from manual to automatic mode.
-     ******************************************************************************/
-    """
-    def Initialize(self):
+
+    def reinitialse(self):
+        """ Does all the things that need to happen to ensure a bumpless transfer
+            from manual to automatic mode.
+        """
         self.ITerm = self.myOutput
         self.lastInput = self.myInput
         if self.ITerm > self.outMax:
@@ -187,31 +210,10 @@ class mrBit_pid:
         elif self.ITerm < self.outMin:
             self.ITerm = self.outMin
 
-    """
-    /* SetControllerDirection(...)*************************************************
-     * The PID will either be connected to a DIRECT acting process (+Output leads
-     * to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
-     * know which one, because otherwise we may increase the output when we should
-     * be decreasing.  This is called from the constructor.
-     ******************************************************************************/
-     """
-    def SetControllerDirection(self,Direction):
-        if self.inAuto and Direction != self.controllerDirection:
-            self.kp = -self.kp
-            self.ki = -self.ki
-            self.kd = -self.kd
-        self.controllerDirection = Direction;
 
-    """
-    /* Status Funcions*************************************************************
-     * Just because you set the Kp=-1 doesn't mean it actually happened.  these
-     * functions query the internal state of the PID.  they're here for display
-     * purposes.  this are the functions the PID Front-end uses for example
-     ******************************************************************************/
-    """
     def GetKp(self): return self.dispKp
     def GetKi(self): return self.dispKi
     def GetKd(self): return self.dispKd
     def GetMode(self): return self.inAuto
     def GetDirection(self): return self.controllerDirection
-    def GetOutput(self): return self.myOutput
+    def GetOutput(self): return self.
